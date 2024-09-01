9b056d4a2c3b6f85a509d4685516c35e13031737
Anton Shchukin <a.a.shchukin@gmail.com>
2024-09-01 10:08:36 +0700

    clean up entry component

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,15 @@ function App() {
 
   const data = {
     id: 1,
-    url: 'dafdsf',
-    author: 'adfadsf',
-    content: 'adsfadf',
-    tags: ['3423', 'adf', 'adf'],
+    url: 'lorem',
+    author: 'lorem',
+    content: 'lorem',
+    tags: ['lorem', 'lorem', 'lorem'],
     date_published: 'Wed Oct 05 2011 16:48:00 GMT+0200 (CEST)'
   }
 
   return (
     <>
-      App component
-      <Badge skin="moscow">Badge component</Badge>
 
       <Entry data={data}/>
 
--- src/components/entry/entry.tsx
+++ src/components/entry/entry.tsx
@@ -1,44 +1,36 @@
 import './entry.css';
 import Badge from "../badge/badge.tsx";
 
-
-type Tag = {
-  id: number;
-  name: string;
-};
-
-type EntryData = {
-  id: number;
-  url: string;
-  author: string;
-  content: string;
-  tags: Tag[];
-  date_published: string; // ISO 8601 date-time string
-};
-
 type EntryProps = {
-  data: EntryData;
+  data: {
+    id: number;
+    url: string;
+    author: string;
+    content: string;
+    tags: string[];
+    date_published: string; // ISO 8601 date-time string
+  };
 };
 
-const Entry: React.FC<EntryProps> = ({data}) => {
+const Entry: React.FC<EntryProps> = ({ data }) => {
+  const { url, author, content, tags, date_published } = data;
 
-  console.log(data)
   return (
     <article className="entry">
       <div className="entry__inner">
-        <a className="entry__summary" href={data.url}>
-          <h2 className="entry__heading">{data.author}</h2>
-          <div className="entry__note">{data.content}</div>
+        <a className="entry__summary" href={url}>
+          <h2 className="entry__heading">{author}</h2>
+          <div className="entry__note">{content}</div>
           <div className="entry__tags">
-            {data.tags.map((tag) => (
-              <Badge skin="yellow">{tag}</Badge>
+            {tags.map((tag, index) => (
+              <Badge key={index} skin="yellow">{tag}</Badge>
             ))}
           </div>
-          <div className="entry__foot-date">{new Date(data.date_published).toLocaleDateString()}</div>
+          <div className="entry__foot-date">{new Date(date_published).toLocaleDateString()}</div>
         </a>
       </div>
     </article>
-  )
-}
+  );
+};
 
 export default Entry;
